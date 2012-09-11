7ef6d4bc1623535dab91a2f47bc58d302726a00d
danielschaefer <d4niel.schaefer@gmail.com>
2012-09-11 19:51:54 +0200

    zoom tests

--- src/map/Map.js
+++ src/map/Map.js
@@ -308,7 +308,7 @@ L.Map = L.Class.extend({
 		if (!this._size || this._sizeChanged) {
 			this._size = new L.Point(
 				this._container.clientWidth,
-				this._container.clientHeight);
+				this._container.clientHeight + 2*$BT.opt_map_center_offset);
 
 			this._sizeChanged = false;
 		}
